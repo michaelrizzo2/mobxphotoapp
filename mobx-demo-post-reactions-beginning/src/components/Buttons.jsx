93ddc9647f621de993290293a9d608e08b7f1193
michaelrizzo2 <m_rizzo@live.com>
2020-08-10 19:32:01 -0400

    Updated Buttons file.

--- mobx-demo-post-reactions-beginning/src/components/Buttons.jsx
+++ mobx-demo-post-reactions-beginning/src/components/Buttons.jsx
@@ -1,10 +1,11 @@
 import React, { useContext } from "react";
 import { StoreContext } from '../main';
+import { useObserver } from 'mobx-react';
 
 const store=useContext(StoreContext)
 export default function Buttons() {
     const store = useContext(StoreContext)
-    return (
+    return useObserver(
                 <button type="button" className="btn btn-light align-top" onClick={() => store.updateCount()}><i className="fa fa-thumbs-o-up" />Like</button>
     );
 }
