e0144bb1f2b2e4900d920c190cd28148b36a12e8
michaelrizzo2 <m_rizzo@live.com>
2020-08-10 18:59:52 -0400

    Store Created.

---
+++ mobx-demo-post-reactions-beginning/src/store/Store.jsx
@@ -0,0 +1,15 @@
+import { observable,action } from "mobx";
+
+class Store
+{
+    @observable likesCount=12
+
+    @action updateCount()
+    {
+        this.likesCount++;
+    }
+}
+
+const storeInstance = new Store()
+export default storeInstance;
+
