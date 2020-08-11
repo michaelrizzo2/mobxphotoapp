4dfefcd659d82200d2b3bac312fc9e22db273778
michaelrizzo2 <m_rizzo@live.com>
2020-08-10 22:17:52 -0400

    Prevented an error.

---
+++ mobx-demo-post-reactions-beginning/src/store/ImageStore.jsx
@@ -0,0 +1,31 @@
+import {action,runInAction,observable} from "mobx";
+
+class ImageStore
+{
+    id=1
+    @observable imageUrl = `https://rickandmortyapi.com/api/character/avatar/1.jpeg`
+    
+    @action async fetchImage() {
+            const characterId = ++this.id
+            const response = await fetch(`https://rickandmortyapi.com/api/character/${characterId}`)
+            const data = await response.json()
+            runInAction(() => {
+                this.imageUrl = data.image
+            })
+    }
+
+     @action async fetchImage() {
+            
+            this.setImageUrl(data.image)
+    }
+
+
+    @action setImageUrl(url) {
+        this.imageUrl = url
+    }
+
+}
+
+const imageStore = new ImageStore()
+
+export default imageStore;
